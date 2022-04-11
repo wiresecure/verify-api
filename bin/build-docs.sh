44a7463bcc02bdfb7b95d2fde345845de0ef1d78
Brian Starke <brian.starke@gmail.com>
2022-04-11 09:51:10 -0400

    rebuild

--- bin/build-docs.sh
+++ bin/build-docs.sh
@@ -3,12 +3,10 @@
 echo "Getting started"
 
 # Bundle docs into zero-dependency HTML file
-npx redoc-cli bundle openapi/openapi.yaml -t docs/index.html \
+npx redoc-cli build openapi/openapi.yaml -t docs/index.html \
 --options.theme.colors.primary.main=#3e7cec \
 --options.theme.colors.secondary.main=#fbbe2f \
---options.theme.typography.fontFamily="nimbus-sans" \
 --options.theme.typography.smoothing="antialiased" \
---options.theme.typography.headings.fontFamily="termina" \
 && \
 mv redoc-static.html index.html && \
 echo "Changed name from redoc-static.html to index.html..." 
